4f274e9e5fe7c6a57ccb4f4717570ef713528cfc
William Roe <willroe@gmail.com>
2012-07-11 00:17:03 +0100

    Use proper chef-isms for /tmp

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,14 +22,14 @@ install_location = "/srv/golang/versions/#{node[:golang][:version]}"
 arch = kernel['machine'] =~ /x86_64/ ? "amd64" : "386"
 version = node[:golang][:version]
 
-remote_file "/tmp/go-#{version}.tar.gz" do
+remote_file "#{Chef::Config[:file_cache_path]}/go-#{version}.tar.gz" do
   source "http://go.googlecode.com/files/go#{version}.linux-#{arch}.tar.gz"
   mode "0644"
   checksum node[:golang][version][arch][:checksum]
-  not_if { ::File.exists?("/tmp/go-#{version}.tar.gz") }
+  not_if { ::File.exists?("#{Chef::Config[:file_cache_path]}/go-#{version}.tar.gz") }
 end
 
-execute "tar -C #{install_location} --strip-components=1 -xzf /tmp/go-#{version}.tar.gz" do
+execute "tar -C #{install_location} --strip-components=1 -xzf #{Chef::Config[:file_cache_path]}/go-#{version}.tar.gz" do
   not_if { ::File.exists?(install_location) }
 end
 
